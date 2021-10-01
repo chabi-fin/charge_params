25b8c8538d4ccb39599c2ca62765b66608ed54ae
chabi-fin <lauren.m.finn@gmail.com>
2021-10-01 11:51:15 +0200

    commandline arguement flags, improved handling of multiple conformations

--- initialize_stage1.sh
+++ initialize_stage1.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
 
-# parse the command line
-NAME=$1
-NETCHARGE=$2
-DIR=$3
-shift 3
-CONFORMS=("$@")
+# Default arguements
+NAME="MOL"
+NETCHARGE=0
+DIR=$(pwd)
+
+# Parse the command line
+while getopts n:c:d:o: flag
+do
+    case "${flag}" in
+	n) NAME=${OPTARG};;
+	c) NETCHARGE=${OPTARG};;
+	d) DIR=${OPTARG};;
+    esac
+done 
+
+# If name.mol2 not in directrory, exit
+usage() { echo "Usage: ./initialize_stage1.sh -n NAME -c CHARGE -d DIRECTORY"; exit 1;}
 
 cd $DIR
 
-echo "Usage: ./initialize_stage1.sh NAME CHARGE PATH Conform1 Conform2 ..."
+# Get all the mol files in an array
+molfiles=`ls ./$NAME_*.mol2`
+molfiles=( $molfiles )
+if [ -z molfiles ]; then
+    usage
+fi
 
-for CONFORM in "${CONFORMS[@]}"; do
-        
-    mkdir -p stage1_${CONFORM}
+# For just one conformation 
+if [ ${#molfiles[@]} -eq 1 ]; then
 
-    cp ${NAME}_${CONFORM}.mol2 stage1_${CONFORM}/${NAME}.mol2
-    cd stage1_${CONFORM}
+    if [ ! -f ${NAME}.mol2 ]; then
+	usage
+    fi
 
-    acpype -di ${NAME}.mol2 -n $NETCHARGE 
+    mkdir -p stage1
+    cp ${NAME}.mol2 stage1
+    cd stage1
+    
+    acpype -di ${NAME}.mol2 -n 
+
+    cp ${NAME}.acpype/${NAME}_NEW.pdb ./${NAME}.pdb
+
+fi
+
+# Multiple primary conformations
+for conform in ${molfiles[@]}; do
+    
+    conform=${conform/.\/${NAME}_/}
+    conform=${conform/.mol2/}
+    mkdir -p stage1_$conform
+
+    cp ${NAME}_${conform}.mol2 stage1_${conform}/${NAME}.mol2
+    cd stage1_${conform}
+
+    acpype -di ${NAME}.mol2 -n $NETCHARGE
 
     cp ${NAME}.acpype/${NAME}_NEW.pdb ./${NAME}.pdb
     cd ..
